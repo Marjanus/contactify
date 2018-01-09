b4e9e42e21e4af31c3dd09e62e5bba3b1c173248
vu20859 <martynas.janusevicius@ittc.vu.lt>
2018-01-09 17:54:20 +0200

    Add initial FiltersBar styling

--- src/containers/FiltersBar.jsx
+++ src/containers/FiltersBar.jsx
@@ -6,22 +6,25 @@ export default class FiltersBar extends Component {
     return (
       <section className="filters-bar">
         <div className="top-container">
-          <form action="" method="get">
-            <input type="text" name="name" placeholder="name" />
-            <select name="city">
-              <option value="">City</option>
-              <option value="london">London</option>
-              <option value="sydney">Sydney</option>
-              <option value="los angeles">Los Angeles</option>
-              <option value="singapore">Singapore</option>
-              <option value="hong kong">Hong Kong</option>
-            </select>
-            <input type="checkbox" name="active" value="true" checked={false} />Show active
-            <button type="submit" onClick={this.props.onFilterContacts}>Filter</button>
-          </form>
-          <button type="button">
-            <i className="fa fa-plus" aria-hidden="true" />Add new contact
-          </button>
+          <div>
+            <form action="" method="get">
+              <input type="text" name="name" placeholder="name" />
+              <select name="city">
+                <option value="">City</option>
+                <option value="london">London</option>
+                <option value="sydney">Sydney</option>
+                <option value="los ang=les">Los Angeles</option>
+                <option value="singapore">Singapore</option>
+                <option value="hong kong">Hong Kong</option>
+              </select>
+              <input type="checkbox" name="active" value="true" checked={false} />Show active
+              <button type="submit" onClick={this.props.onFilterContacts}>Filter</button>
+            </form>
+            <button type="button" className="add-contact">
+              <i className="fa fa-plus" aria-hidden="true" />
+              <span>ADD NEW CONTACT</span>
+            </button>
+          </div>
         </div>
       </section>
     );
